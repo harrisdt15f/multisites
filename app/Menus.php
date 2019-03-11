a8801a38fe946376d47edfd64320517dc6017d3c
harrisdt15f <harrisdt15f@gmail.com>
2019-03-11 20:43:02 +0800

    :sparkles: menu cache put into redis

--- app/Menus.php
+++ app/Menus.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use LaravelArdent\Ardent\Ardent;
+use phpDocumentor\Reflection\Types\Self_;
 
 class Menus extends Ardent
 {
@@ -16,10 +18,41 @@ class Menus extends Ardent
      */
     public static function menuLists()
     {
-        $hourToStore=24;
+        $hourToStore = 24;
         $parent_menu = [];
-        if (Cache::has('ms_menus')) {
-            $parent_menu = Cache::get('ms_menus');
+        $groupId = Auth::user()->group_id;
+        if ($groupId === 1) {
+            $parent_menu = self::forStar();
+        } else {
+            /*if (Cache::has('ms_menus')) {
+                $parent_menu = Cache::get('ms_menus');
+            } else {
+                $menuLists = self::all();
+                foreach ($menuLists as $key => $value) {
+                    if ($value->pid === 0) {
+                        $parent_menu[$value->id]['label'] = $value->label;
+                        $parent_menu[$value->id]['route'] = $value->route;
+                        $parent_menu[$value->id]['class'] = $value->class;
+                        $parent_menu[$value->id]['pid'] = $value->pid;
+                    } else {
+                        $parent_menu[$value->pid]['child'][$value->id]['label'] = $value->label;
+                        $parent_menu[$value->pid]['child'][$value->id]['route'] = $value->route;
+                        $parent_menu[$value->pid]['child'][$value->id]['class'] = $value->class;
+                        $parent_menu[$value->pid]['child'][$value->id]['pid'] = $value->pid;
+                    }
+                }
+                $expiresAt = Carbon::now()->addHours($hourToStore)->diffInMinutes();
+                Cache::put('ms_menus', $parent_menu, $expiresAt);
+            }*/
+        }
+        return $parent_menu;
+    }
+
+    public static function forStar()
+    {
+        $parent_menu = [];
+        if (Cache::has('ms_menus.*')) {
+            $parent_menu = Cache::get('ms_menus.*');
         } else {
             $menuLists = self::all();
             foreach ($menuLists as $key => $value) {
@@ -35,8 +68,7 @@ class Menus extends Ardent
                     $parent_menu[$value->pid]['child'][$value->id]['pid'] = $value->pid;
                 }
             }
-            $expiresAt = Carbon::now()->addHours($hourToStore)->diffInMinutes();
-            Cache::put('ms_menus', $parent_menu, $expiresAt);
+            Cache::forever('ms_menus.*', $parent_menu);
         }
         return $parent_menu;
     }
