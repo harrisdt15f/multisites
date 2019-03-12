78337a9c1a1e058bfb6d1e0655e2d6e14511674f
harrisdt15f <harrisdt15f@gmail.com>
2019-03-12 22:14:55 +0800

    :sparkles: generate menus with formula

--- resources/views/common/asideleft.blade.php
+++ resources/views/common/asideleft.blade.php
@@ -1,48 +1,54 @@
 <!-- BEGIN: Left Aside -->
-{{--{{dd($menulists)}}--}}
 <button class="m-aside-left-close  m-aside-left-close--skin-dark " id="m_aside_left_close_btn"><i
         class="la la-close"></i></button>
 <div id="m_aside_left" class="m-grid__item	m-aside-left  m-aside-left--skin-dark ">
 
     <!-- BEGIN: Aside Menu -->
+    <?php
+    $parentItems = <<<parentitem
+<li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover"><a
+                    href="javascript:;" class="m-menu__link m-menu__toggle"><i
+                        class="m-menu__link-icon flaticon-layers"></i><span
+                        class="m-menu__link-text">~plabel~</span><i
+                        class="m-menu__ver-arrow la la-angle-right"></i></a>
+~inner~
+</li>
+parentitem;
+    $innerItems = <<<inner
+ <div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+                    <ul class="m-menu__subnav">
+                        <li class="m-menu__item  m-menu__item--parent" aria-haspopup="true"><span
+                                class="m-menu__link"><span class="m-menu__link-text">~plabel~</span></span></li>
+                        <li class="m-menu__item " aria-haspopup="true">
+                            <a href="~iroute~" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span
+                                    class="m-menu__link-text">~ilabel~</span></a></li>
+                    </ul>
+                </div>
+inner;
+    //Generate menus
+    $menuItems = $tempParentItems = $tempInnerItems = '';
+    foreach ($menulists as $value) {
+        if ($value['pid'] === 0) {
+            $tempParentItems = $tempParentItems === '' ? $parentItems : $tempParentItems . $parentItems;
+            $tempParentItems = str_replace('~plabel~', $value['label'], $tempParentItems);
+            if (array_key_exists('child', $value)) {
+                foreach ($value['child'] as $_value) {
+                    $tempInnerItems = $tempInnerItems === '' ? $innerItems : $tempInnerItems . $innerItems;
+                    $tempInnerItems = str_replace('~plabel~', $value['label'], $tempInnerItems);
+                    $tempInnerItems = str_replace('~ilabel~', $_value['label'], $tempInnerItems);
+                    $tempInnerItems = $_value['route'] === '#' ? str_replace('~iroute~', '#', $tempInnerItems) : str_replace('~iroute~', route($_value['route']), $tempInnerItems);
+                }
+            }
+            $tempMenuItems = str_replace('~inner~', $tempInnerItems, $tempParentItems);
+            $menuItems = $menuItems === '' ? $tempMenuItems : $menuItems . $tempMenuItems;
+            $tempMenuItems = $tempParentItems = $tempInnerItems = '';
+        }
+    }
+    ?>
     <div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark "
          m-menu-vertical="1" m-menu-scrollable="1" m-menu-dropdown-timeout="500" style="position: relative;">
         <ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
-
-            <?php
-            foreach ($menulists as $key => $value) {
-            if ($value['pid'] === 0)
-            {
-            ?>
-            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover"><a
-                    href="javascript:;" class="m-menu__link m-menu__toggle"><i
-                        class="m-menu__link-icon flaticon-layers"></i><span
-                        class="m-menu__link-text">{{$value['label']}}</span><i
-                        class="m-menu__ver-arrow la la-angle-right"></i></a>
-                <?php
-                }
-                if (array_key_exists('child', $value)) {
-                foreach ($value['child'] as $_key => $_value) {
-                ?>
-                <div class="m-menu__submenu "><span class="m-menu__arrow"></span>
-                    <ul class="m-menu__subnav">
-                        <li class="m-menu__item  m-menu__item--parent" aria-haspopup="true"><span
-                                class="m-menu__link"><span class="m-menu__link-text">{{$value['label']}}</span></span></li>
-                        <li class="m-menu__item " aria-haspopup="true">
-                            <a href="@if ($_value['route'] === '#')#@else
-                                {{route($_value['route'])}}
-                            @endif" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span
-                                    class="m-menu__link-text">{{$_value['label']}}</span></a></li>
-                    </ul>
-                </div>
-                <?php
-                }
-                }
-                ?>
-            </li>
-            <?php
-            }
-            ?>
+            {!! $menuItems !!}
         </ul>
     </div>
 
--- resources/views/superadmin/menu-setting/index.blade.php
+++ resources/views/superadmin/menu-setting/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="m-portlet__head-caption">
                     <div class="m-portlet__head-title">
                         <h3 class="m-portlet__head-text">
-                            Basic Tree
+                            菜单详情
                         </h3>
                     </div>
                 </div>
@@ -19,89 +19,19 @@
             <div class="m-portlet__body">
                 <div id="m_tree_1" class="tree-demo">
                     <ul>
-                        <li>
+                        <li data-jstree='{ "opened" : true }'>
                             Root node 1
                             <ul>
-                                <li data-jstree='{ "selected" : true }'>
-                                    <a href="javascript:;">
-                                        Initially selected </a>
+                                <li data-jstree='{ "icon" : "fa fa-briefcase m--font-success " }'>
+                                    custom icon URL
                                 </li>
                                 <li data-jstree='{ "icon" : "fa fa-briefcase m--font-success " }'>
                                     custom icon URL
                                 </li>
-                                <li data-jstree='{ "opened" : true }'>
-                                    initially open
-                                    <ul>
-                                        <li data-jstree='{ "disabled" : true }'>
-                                            Disabled Node
-                                        </li>
-                                        <li data-jstree='{ "type" : "file" }'>
-                                            Another node
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li data-jstree='{ "icon" : "fa fa-warning m--font-danger" }'>
-                                    Custom icon class (bootstrap)
-                                </li>
-                            </ul>
-                        </li>
-                        <li data-jstree='{ "type" : "file" }'>
-                            <a href="http://www.keenthemes.com">
-                                Clickanle link node </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="alert alert-success m--margin-top-10">
-                    Note! Opened and selected nodes will be saved in the user's browser, so when returning to the same tree the previous state will be restored.
-                </div>
-            </div>
-        </div>
-
-        <!--end::Portlet-->
-
-        <!--begin::Portlet-->
-        <div class="m-portlet">
-            <div class="m-portlet__head">
-                <div class="m-portlet__head-caption">
-                    <div class="m-portlet__head-title">
-                        <h3 class="m-portlet__head-text">
-                            Custom Icons & Clickable Nodes
-                        </h3>
-                    </div>
-                </div>
-            </div>
-            <div class="m-portlet__body">
-                <div id="m_tree_2" class="tree-demo">
-                    <ul>
-                        <li>
-                            Root node 1
-                            <ul>
-                                <li data-jstree='{ "selected" : true }'>
-                                    <a href="javascript:;">
-                                        Initially selected </a>
-                                </li>
                                 <li data-jstree='{ "icon" : "fa fa-briefcase m--font-success " }'>
                                     custom icon URL
                                 </li>
-                                <li data-jstree='{ "opened" : true }'>
-                                    initially open
-                                    <ul>
-                                        <li data-jstree='{ "disabled" : true }'>
-                                            Disabled Node
-                                        </li>
-                                        <li data-jstree='{ "type" : "file" }'>
-                                            Another node
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li data-jstree='{ "icon" : "fa fa-warning m--font-danger" }'>
-                                    Custom icon class (bootstrap)
-                                </li>
                             </ul>
-                        </li>
-                        <li data-jstree='{ "type" : "file" }'>
-                            <a href="http://www.jstree.com">
-                                Clickanle link node </a>
                         </li>
                     </ul>
                 </div>
@@ -111,7 +41,7 @@
         <!--end::Portlet-->
 
         <!--begin::Portlet-->
-        <div class="m-portlet">
+        {{--<div class="m-portlet">
             <div class="m-portlet__head">
                 <div class="m-portlet__head-caption">
                     <div class="m-portlet__head-title">
@@ -125,7 +55,7 @@
                 <div id="m_tree_3" class="tree-demo">
                 </div>
             </div>
-        </div>
+        </div>--}}
 
         <!--end::Portlet-->
     </div>
