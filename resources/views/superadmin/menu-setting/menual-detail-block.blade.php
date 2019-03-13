dd2ec3414f88b28f560f1a1c408a209095f9db3e
harrisdt15f <harrisdt15f@gmail.com>
2019-03-13 22:52:27 +0800

    :sparkles: detail move submit had finised

--- resources/views/superadmin/menu-setting/menual-detail-block.blade.php
+++ resources/views/superadmin/menu-setting/menual-detail-block.blade.php
@@ -1,22 +1,22 @@
 <?php
 $state = [
-    'id'=>'',
+    'id' => '',
     'text' => '',
-    'nodeId'=> '',
+    'nodeId' => '',
     'children' => [],
     'state' => ['opened' => true]
 ];
 $arrTree = [];
 foreach ($menulists as $key => $value) {
     if ($value['pid'] === 0) {
-       $temp = $state;
-       $temp['id']=$key;
-       $temp['text']=$value['label'];
+        $temp = $state;
+        $temp['id'] = $key;
+        $temp['text'] = $value['label'];
 //        $arrTree[]['text'] = ;
         if (array_key_exists('child', $value)) {
             foreach ($value['child'] as $_key => $_value) {
-                $tempChild['text']=$_value['label'];
-                $tempChild['id']=$_key;
+                $tempChild['text'] = $_value['label'];
+                $tempChild['id'] = $_key;
                 $temp['children'][] = $tempChild;
             }
         }
@@ -67,8 +67,8 @@ $treejson = json_encode($arrTree, JSON_UNESCAPED_UNICODE);
                         'data': {!! $treejson !!},
                     },
                     "types": {
-                        "#" : {
-                            "max_depth" : 2,
+                        "#": {
+                            "max_depth": 2,
                         },
                         "default": {
                             "icon": "fa fa-folder m--font-success"
@@ -80,6 +80,30 @@ $treejson = json_encode($arrTree, JSON_UNESCAPED_UNICODE);
                     "state": {"key": "demo2"},
                     "plugins": ["dnd", "state", "types"]
                 });
+
+                $("#m_tree_1")
+                    .on('move_node.jstree', function (e, data) {
+                        console.info(data);
+                        var dragResult = $("#dragResult").val();
+                        console.log(dragResult);
+                        if (dragResult != "") {
+                            datas = jQuery.parseJSON(dragResult);
+                        } else {
+                            datas = {};
+                        }
+                        currentId = data.node.id;
+                        oldParent = data.old_parent;
+                        currentParent = data.parent;
+                        currentText = data.node.text;
+                        datas[currentId] = {};
+                        datas[currentId]['currentId'] = currentId;
+                        datas[currentId]['oldParent'] = oldParent;
+                        datas[currentId]['currentParent'] = currentParent;
+                        datas[currentId]['currentText'] = currentText;
+                        $("#dragResult").val(JSON.stringify(datas));
+                        $('#detail-submit').removeAttr('disabled')
+                        console.log(datas);
+                    })
             }
         @stop
         @section('demo1-tree-return')
