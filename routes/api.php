caaf8468862b03e59b6f7ebcb0ee2408a241c2c4
harrisdt15f <harrisdt15f@gmail.com>
2019-04-03 16:33:52 +0800

    :sparkles: 所有route 都添加option

--- routes/api.php
+++ routes/api.php
@@ -14,18 +14,17 @@ use Illuminate\Http\Request;
 */
 //Auth::routes();
 Route::group(['middleware' => 'api','namespace' => 'API'], function(){
-    Route::match(['post','options'],'login','AuthController@login');
+    Route::match(['post','options'],'login', ['as' => 'login', 'uses' => 'AuthController@login']);
     Route::post('register','AuthController@register');
 });
 Route::group(['middleware' => ['api','auth:api'],'namespace' => 'API'], function(){
     //商户用户相关
     Route::post('details', ['as' => 'detail', 'uses' => 'AuthController@details']);
-    Route::get('logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
     Route::get('user', ['as' => 'user', 'uses' => 'AuthController@user']);
 
+    Route::match(['get','options'],'logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
     //菜单相关
-    Route::get('menu/get-all-menu', ['as' => 'menu.allPartnerMenu', 'uses' => 'MenuController@getAllMenu']);
-
+    Route::match(['get','options'],'menu/get-all-menu', ['as' => 'menu.allPartnerMenu', 'uses' => 'MenuController@getAllMenu']);
     //用户组相关
-    Route::post('partner-admin-group/create', ['as' => 'partnerAdminGroup.create', 'uses' => 'PartnerAdminGroupController@create']);
+    Route::match(['post','options'],'partner-admin-group/create', ['as' => 'partnerAdminGroup.create', 'uses' => 'PartnerAdminGroupController@create']);
 });
