dd3b210909b6cf09c6ec21165f662044eaa07837
LingPh <953909858@qq.com>
2019-04-19 14:49:30 +0800

    修改配置/删除配置 功能修复

--- app/Http/Controllers/API/ConfiguresController.php
+++ app/Http/Controllers/API/ConfiguresController.php
@@ -5,106 +5,121 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\ApiMainController;
 use Illuminate\Support\Facades\Validator;
 
-class ConfiguresController extends ApiMainController
-{
-    protected $eloqM = 'PartnerSysConfigures';
+class ConfiguresController extends ApiMainController {
+	protected $eloqM = 'PartnerSysConfigures';
 
-    //获取全部配置
-    public function getConfiguresList()
-    {
-        $all = $this->eloqM::all()->toArray();
-        foreach ($all as $k1 => $v1) {
-            if ($all[$k1]['parent_id'] == 0) {
-                $data[] = $all[$k1];
-            } else {
-                foreach ($data as $k2 => $v2) {
-                    if ($all[$k1]['parent_id'] == $data[$k2]['id']) {
-                        $data[$k2]['sub'][] = $all[$k1];
-                    }
-                }
-            }
-        }
-        return $this->msgout(true, $data);
-    }
+	//获取全部配置
+	public function getConfiguresList() {
+		$all = $this->eloqM::all()->toArray();
+		foreach ($all as $k1 => $v1) {
+			if ($all[$k1]['parent_id'] == 0) {
+				$data[] = $all[$k1];
+			} else {
+				foreach ($data as $k2 => $v2) {
+					if ($all[$k1]['parent_id'] == $data[$k2]['id']) {
+						$data[$k2]['sub'][] = $all[$k1];
+					}
+				}
+			}
+		}
+		return $this->msgout(true, $data);
+	}
 
-    //添加配置
-    public function add()
-    {
-        $validator = Validator::make($this->inputs, [
-            'parent_id' => 'required|numeric',
-            'sign' => 'required',
-            'name' => 'required',
-            'description' => 'required',
-            'value' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return $this->msgout(false, [], $validator->errors()->first(), 200);
-        }
-        $addDatas = [
-            'parent_id' => $this->inputs['parent_id'],
-            'sign' => $this->inputs['sign'],
-            'name' => $this->inputs['name'],
-            'description' => $this->inputs['description'],
-            'value' => $this->inputs['value'],
-            'pid' => $this->currentPlatformEloq->platform_id,
-            'add_admin_id' => $this->partnerAdmin->id,
-            'last_update_admin_id' => $this->partnerAdmin->id,
-            'status' => 1,
-        ];
-        $pastData = $this->eloqM::where('sign', $this->inputs['sign'])->first();
-        if (is_null($pastData)) {
-            try {
-                $configure = new $this->eloqM();
-                $configure->fill($addDatas);
-                $configure->save();
-                return $this->msgout(true, [], '添加配置成功');
-            } catch (\Exception $e) {
-                $errorObj = $e->getPrevious()->getPrevious();
-                [$sqlState, $errorCode, $msg] = $errorObj->errorInfo; //［sql编码,错误妈，错误信息］
-                return $this->msgout(false, [], $msg, $sqlState);
-            }
-        } else {
-            return $this->msgout(false, [], '该配置已有请', '0009');
-        }
-    }
+	//添加配置
+	public function add() {
+		$validator = Validator::make($this->inputs, [
+			'parent_id' => 'required|numeric',
+			'sign' => 'required',
+			'name' => 'required',
+			'description' => 'required',
+			'value' => 'required',
+		]);
+		if ($validator->fails()) {
+			return $this->msgout(false, [], $validator->errors()->first(), 200);
+		}
+		$addDatas = [
+			'parent_id' => $this->inputs['parent_id'],
+			'sign' => $this->inputs['sign'],
+			'name' => $this->inputs['name'],
+			'description' => $this->inputs['description'],
+			'value' => $this->inputs['value'],
+			'pid' => $this->currentPlatformEloq->platform_id,
+			'add_admin_id' => $this->partnerAdmin->id,
+			'last_update_admin_id' => $this->partnerAdmin->id,
+			'status' => 1,
+		];
+		$pastData = $this->eloqM::where('sign', $this->inputs['sign'])->first();
+		if (is_null($pastData)) {
+			try {
+				$configure = new $this->eloqM();
+				$configure->fill($addDatas);
+				$configure->save();
+				return $this->msgout(true, [], '添加配置成功');
+			} catch (\Exception $e) {
+				$errorObj = $e->getPrevious()->getPrevious();
+				[$sqlState, $errorCode, $msg] = $errorObj->errorInfo; //［sql编码,错误妈，错误信息］
+				return $this->msgout(false, [], $msg, $sqlState);
+			}
+		} else {
+			return $this->msgout(false, [], '该配置已有请', '0009');
+		}
+	}
 
-    //修改配置
-    public function edit()
-    {
-        if ($this->inputs) {
-            $id = $this->inputs['id'];
-            $edit = [
-                'sign' => $this->inputs['sign'],
-                'name' => $this->inputs['name'],
-                'description' => $this->inputs['description'],
-            ];
-            //不是顶级可修改值
-            if ($this->inputs['parent_id'] != 0) {
-                $edit['value'] = $this->inputs['value'];
-            }
-            $sqlstatus = $this->eloqM::where('id', '=', $id)->update($edit);
-            if ($sqlstatus) {
-                return $this->msgout(true, [], '修改配置成功', '200');
-            } else {
-                return $this->msgout(false, [], '修改配置失败', '0002');
-            }
-        } else {
-            return $this->msgout(false, [], '请传递参数', '0002');
-        }
-    }
+	//修改配置
+	public function edit() {
+		$validator = Validator::make($this->inputs, [
+			'id' => 'required|numeric',
+			'parent_id' => 'required|numeric',
+			'sign' => 'required',
+			'name' => 'required',
+			'description' => 'required',
+		]);
+		if ($validator->fails()) {
+			return $this->msgout(false, [], $validator->errors()->first(), 200);
+		}
+		$pastData = $this->eloqM::where('sign', $this->inputs['sign'])->first();
+		if (is_null($pastData)) {
+			$editDataEloq = $this->eloqM::find($this->inputs['id']);
+			$editDataEloq->sign = $this->inputs['sign'];
+			$editDataEloq->name = $this->inputs['name'];
+			$editDataEloq->description = $this->inputs['description'];
+			//不是顶级可修改值
+			if ($this->inputs['parent_id'] != 0) {
+				$editDataEloq->value = $this->inputs['value'];
+			}
+			try {
+				$editDataEloq->save();
+				return $this->msgout(true, [], '修改配置成功');
+			} catch (\Exception $e) {
+				$errorObj = $e->getPrevious()->getPrevious();
+				[$sqlState, $errorCode, $msg] = $errorObj->errorInfo; //［sql编码,错误码，错误信息］
+				return $this->msgout(false, [], $msg, $sqlState);
+			}
+		} else {
+			return $this->msgout(false, [], '该配置已有请', '0009');
+		}
+	}
 
-    //删除配置
-    public function delete()
-    {
-        if ($this->inputs['id']) {
-            $sqlstatus = $this->eloqM::where('id', '=', $this->inputs['id'])->delete();
-            if ($sqlstatus) {
-                return $this->msgout(true, [], '删除配置成功', '200');
-            } else {
-                return $this->msgout(false, [], '删除配置失败', '0002');
-            }
-        } else {
-            return $this->msgout(false, [], '请传递参数', '0002');
-        }
-    }
+	//删除配置
+	public function delete() {
+		$validator = Validator::make($this->inputs, [
+			'id' => 'required|numeric',
+		]);
+		if ($validator->fails()) {
+			return $this->msgout(false, [], $validator->errors()->first(), 200);
+		}
+		$pastData = $this->eloqM::find($this->inputs['id']);
+		if (!is_null($pastData)) {
+			try {
+				$this->eloqM::where('id', $this->inputs['id'])->delete();
+				return $this->msgout(true, [], '删除配置成功', '200');
+			} catch (\Exception $e) {
+				$errorObj = $e->getPrevious()->getPrevious();
+				[$sqlState, $errorCode, $msg] = $errorObj->errorInfo; //［sql编码,错误码，错误信息］
+				return $this->msgout(false, [], $msg, $sqlState);
+			}
+		} else {
+			return $this->msgout(false, [], '该数据不存在', '0009');
+		}
+	}
 }
