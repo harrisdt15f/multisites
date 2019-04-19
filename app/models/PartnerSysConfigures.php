259e259234a1bf0acae305d35c401115953d14fa
harrisdt15f <harrisdt15f@gmail.com>
2019-04-19 12:02:12 +0800

    :sparkles: 添加配置功能修复 问题

--- app/models/PartnerSysConfigures.php
+++ app/models/PartnerSysConfigures.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class PartnerSysConfigures extends BaseModel
 {
     protected $table = 'partner_sys_configures';
+
+    protected $fillable = [
+        'parent_id', 'pid', 'sign', 'name', 'description', 'value', 'add_admin_id', 'last_update_admin_id', 'status'
+    ];
 }
