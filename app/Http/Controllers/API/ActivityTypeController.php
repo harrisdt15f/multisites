e7ea4c68714a442883372a1a18b3320bd6639311
LingPh <953909858@qq.com>
2019-04-25 15:20:26 +0800

    代码PSR规范

--- app/Http/Controllers/API/ActivityTypeController.php
+++ app/Http/Controllers/API/ActivityTypeController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\ApiMainController;
 use Illuminate\Support\Facades\Validator;
 
-class ActivityTypeController extends ApiMainController {
-	protected $eloqM = 'ActivityType';
-	public function detail() {
-		$datas = $this->eloqM::where('status', 1)->get()->toArray();
-		if (empty($datas)) {
-			return $this->msgout(false, [], '没有获取到数据', '0009');
-		}
-		return $this->msgout(true, $datas);
-	}
+class ActivityTypeController extends ApiMainController
+{
+    protected $eloqM = 'ActivityType';
+    public function detail()
+    {
+        $datas = $this->eloqM::where('status', 1)->get()->toArray();
+        if (empty($datas)) {
+            return $this->msgout(false, [], '没有获取到数据', '0009');
+        }
+        return $this->msgout(true, $datas);
+    }
 }
