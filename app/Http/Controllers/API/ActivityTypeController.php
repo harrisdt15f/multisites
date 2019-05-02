e2628827ba8ca080a2c44f8ea1c78faa9846157a
LingPh <953909858@qq.com>
2019-05-02 10:52:23 +0800

    活动管理-活动分类 功能

--- app/Http/Controllers/API/ActivityTypeController.php
+++ app/Http/Controllers/API/ActivityTypeController.php
@@ -16,4 +16,49 @@ class ActivityTypeController extends ApiMainController
         }
         return $this->msgout(true, $datas);
     }
+    /**
+     * 编辑活动分类
+     */
+    public function editActype()
+    {
+        $validator = Validator::make($this->inputs, [
+            'id' => 'required|numeric',
+            'status' => 'required|in:0,1',
+            'l_size' => 'numeric|gt:0',
+            'w_size' => 'numeric|gt:0',
+            'size' => 'required|numeric|gt:0',
+        ]);
+        if ($validator->fails()) {
+            return $this->msgout(false, [], $validator->errors()->first());
+        }
+        $editData = $this->eloqM::find($this->inputs['id']);
+        if (is_null($editData)) {
+            return $this->msgout(false, [], '需要修改的活动分类id不存在');
+        }
+        //图片类型的分类
+        if ($editData->ext_type === 1 || $editData->ext_type === 3) {
+            if (!array_key_exists('l_size', $this->inputs) || !array_key_exists('w_size', $this->inputs)) {
+                return $this->msgout(false, [], '图片类型的分类l_size和w_size不能为空');
+            }
+            $editData->l_size = $this->inputs['l_size'];
+            $editData->w_size = $this->inputs['w_size'];
+        }
+        $editData->status = $this->inputs['status'];
+        $editData->size = $this->inputs['size'];
+        try {
+            $editData->save();
+            return $this->msgout(true, [], '修改活动分类成功');
+        } catch (Exception $e) {
+            $errorObj = $e->getPrevious()->getPrevious();
+            [$sqlState, $errorCode, $msg] = $errorObj->errorInfo; //［sql编码,错误码，错误信息］
+            return $this->msgout(false, [], $msg, $sqlState);
+        }
+    }
+    //===============================================
+    //ALTER TABLE `partner_activity_lists`
+    //ADD `ext_type` TINYINT(4),
+    //ADD `l_size` INT,
+    //ADD `w_size` INT,
+    //ADD `size` INT;
+    //===============================================
 }
--- routes/admin/partner_activity.php
+++ routes/admin/partner_activity.php
@@ -2,12 +2,13 @@
 
 //活动相关
 Route::group(['prefix' => 'activity'], function () {
-	$namePrefix = 'activity.';
-	$controller = 'ActivityInfosController@';
-	Route::match(['post', 'options'], 'detail', ['as' => $namePrefix . 'detail', 'uses' => $controller . 'detail']);
-	Route::match(['post', 'options'], 'add', ['as' => $namePrefix . 'add', 'uses' => $controller . 'add']);
-	Route::match(['post', 'options'], 'edit', ['as' => $namePrefix . 'edit', 'uses' => $controller . 'edit']);
-	Route::match(['post', 'options'], 'delete', ['as' => $namePrefix . 'delete', 'uses' => $controller . 'delete']);
-	//活动类型列表
-	Route::match(['get', 'options'], 'type', ['as' => $namePrefix . 'type', 'uses' => 'ActivityTypeController@detail']);
+    $namePrefix = 'activity.';
+    $controller = 'ActivityInfosController@';
+    Route::match(['post', 'options'], 'detail', ['as' => $namePrefix . 'detail', 'uses' => $controller . 'detail']);
+    Route::match(['post', 'options'], 'add', ['as' => $namePrefix . 'add', 'uses' => $controller . 'add']);
+    Route::match(['post', 'options'], 'edit', ['as' => $namePrefix . 'edit', 'uses' => $controller . 'edit']);
+    Route::match(['post', 'options'], 'delete', ['as' => $namePrefix . 'delete', 'uses' => $controller . 'delete']);
+    //活动类型列表
+    Route::match(['get', 'options'], 'type', ['as' => $namePrefix . 'type', 'uses' => 'ActivityTypeController@detail']);
+    Route::match(['post', 'options'], 'edit-actype', ['as' => $namePrefix . 'edit-actype', 'uses' => 'ActivityTypeController@editActype']);
 });
