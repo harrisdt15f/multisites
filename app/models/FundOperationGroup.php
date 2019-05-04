b8317d04e9b636bf79d9ab60722990ce0cb80355
LingPh <953909858@qq.com>
2019-05-04 18:04:08 +0800

    资金操作-额度管理 API

---
+++ app/models/FundOperationGroup.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\models;
+
+class FundOperationGroup extends BaseModel
+{
+    protected $table = 'fund_operation_group';
+
+    protected $fillable = [
+        'group_id',
+    ];
+}
