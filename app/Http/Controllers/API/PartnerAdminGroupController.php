9a79eb1a67032a6389bf5b09862516e1969b51c0
LingPh <953909858@qq.com>
2019-05-08 15:23:37 +0800

    编辑管理员角色-人工充值权限 修复

--- app/Http/Controllers/API/PartnerAdminGroupController.php
+++ app/Http/Controllers/API/PartnerAdminGroupController.php
@@ -50,7 +50,7 @@ class PartnerAdminGroupController extends ApiMainController
         $objPartnerAdminGroup = new $this->eloqM;
         $objPartnerAdminGroup->fill($data);
         //检查是否有人工充值权限
-        $FundOperation = PartnerMenus::select('id')->where('route', '/manage/recharge')->first()->toArray();
+        $FundOperation = PartnerMenus::select('id')->orWhere('route', '/manage')->orWhere('route', '/manage/prize-manage')->orWhere('route', '/manage/recharge')->get()->toArray();
         $fool = in_array($FundOperation['id'], $role);
         try {
             $objPartnerAdminGroup->save();
@@ -104,7 +104,7 @@ class PartnerAdminGroupController extends ApiMainController
         $datas = $this->eloqM::find($id);
         //检查提交的权限中 是否有 人工充值权限  $fool
         $role = json_decode($this->inputs['role']);
-        $FundOperation = PartnerMenus::select('id')->where('route', '/manage/recharge')->first()->toArray();
+        $FundOperation = PartnerMenus::select('id')->orWhere('route', '/manage')->orWhere('route', '/manage/prize-manage')->orWhere('route', '/manage/recharge')->get()->toArray();
         $fool = in_array($FundOperation['id'], $role);
         if (!is_null($datas)) {
             DB::beginTransaction();
@@ -179,7 +179,7 @@ class PartnerAdminGroupController extends ApiMainController
         ])->first();
         //检查是否有人工充值权限
         $role = json_decode($datas->role);
-        $FundOperation = PartnerMenus::select('id')->where('route', '/manage/recharge')->first()->toArray();
+        $FundOperation = PartnerMenus::select('id')->orWhere('route', '/manage')->orWhere('route', '/manage/prize-manage')->orWhere('route', '/manage/recharge')->get()->toArray();
         $fool = in_array($FundOperation['id'], $role);
         if (!is_null($datas)) {
             try {
