8c21962b96eca2b2e4c8cee0e8e3a22e2d732a96
LingPh <953909858@qq.com>
2019-05-09 20:10:58 +0800

    开发管理-路由管理 功能

--- app/models/PartnerAdminRoute.php
+++ app/models/PartnerAdminRoute.php
@@ -6,8 +6,11 @@ class PartnerAdminRoute extends BaseModel
 {
     protected $table = 'partner_admin_route';
 
+    protected $fillable = [
+        'route_name', 'menu_group_id', 'title', 'description', 'created_at', 'updated_at',
+    ];
     public function menu()
     {
-        return $this->belongsTo(PartnerMenus::class,'menu_group_id','id');
+        return $this->belongsTo(PartnerMenus::class, 'menu_group_id', 'id');
     }
 }
