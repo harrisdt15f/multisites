215aac9c18fc0228811822374f2f9c1f157feb89
harrisdt15f <harrisdt15f@gmail.com>
2019-05-13 15:26:30 +0800

    :sparkles: api namespace was changed to backend api namespace for splitting backend and frontend

--- app/Http/Controllers/BackendApi/RechargeCheckController.php
+++ app/Http/Controllers/BackendApi/RechargeCheckController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\BackendApi;
 
 use App\Http\Controllers\ApiMainController;
 use App\models\AccountChangeReport;
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 |
 */
 //Auth::routes();
-Route::group(['middleware' => ['api'], 'namespace' => 'API'], function () {
+Route::group(['middleware' => ['api'], 'namespace' => 'BackendApi'], function () {
     Route::match(['post', 'options'], 'login', ['as' => 'login', 'uses' => 'AuthController@login']);
 });
 Route::group(['middleware' => ['api', 'auth:admin'], 'namespace' => 'API'], function () {
