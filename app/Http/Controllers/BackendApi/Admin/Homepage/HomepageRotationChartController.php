4c6d47a205571810d5a95e07d093e9ccd4866012
LingPh <953909858@qq.com>
2019-05-20 20:08:20 +0800

    首页管理-轮播图 拖动排序

--- app/Http/Controllers/BackendApi/Admin/Homepage/HomepageRotationChartController.php
+++ app/Http/Controllers/BackendApi/Admin/Homepage/HomepageRotationChartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\BackendApi\BackEndApiMainController;
 use App\lib\Common\Image;
 use App\models\ActivityInfos;
 use App\models\AdvertisementType;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class HomepageRotationChartController extends BackEndApiMainController
@@ -48,10 +49,12 @@ class HomepageRotationChartController extends BackEndApiMainController
                 return $this->msgOut(false, [], '101802');
             }
         }
+
         try {
-            $path = 'uploaded_files/' . $this->currentPlatformEloq->platform_name . '_' . $this->currentPlatformEloq->platform_id . '/Homepagec_Rotation_chart_' . $this->currentPlatformEloq->platform_name . '_' . $this->currentPlatformEloq->platform_id;
             $ImageClass = new Image();
-            $pic = $ImageClass->uploadImg($this->inputs['pic'], $path);
+            $folderName = 'Homepagec_Rotation_chart';
+            $depositPath = $ImageClass->depositPath($folderName, $this->currentPlatformEloq->platform_id, $this->currentPlatformEloq->platform_name);
+            $pic = $ImageClass->uploadImg($this->inputs['pic'], $depositPath);
             if ($pic['success'] === false) {
                 return $this->msgOut(false, [], '101803');
             }
@@ -60,6 +63,13 @@ class HomepageRotationChartController extends BackEndApiMainController
             unset($addData['pic']);
             $addData['pic_path'] = '/' . $pic['path'];
             $addData['thumbnail_path'] = '/' . $thumbnail;
+            //sort
+            $maxSort = $this->eloqM::orderBy('sort', 'desc')->first();
+            if (is_null($maxSort)) {
+                $addData['sort'] = 1;
+            } else {
+                $addData['sort'] = $maxSort->sort + 1;
+            }
             $rotationChartEloq = new $this->eloqM;
             $rotationChartEloq->fill($addData);
             $rotationChartEloq->save();
@@ -108,22 +118,22 @@ class HomepageRotationChartController extends BackEndApiMainController
                 return $this->msgOut(false, [], '101802');
             }
         }
+        $editData = $this->inputs;
+        unset($editData['id']);
+        unset($editData['pic']);
         //如果上传了新图片   就替换原图
-        $ImageClass = new Image();
         if (array_key_exists('pic', $this->inputs)) {
+            $ImageClass = new Image();
             $picData = $this->replaceImage($pastData['pic_path'], $pastData['thumbnail_path'], $this->inputs['pic'], $ImageClass);
-        }
-        if ($picData['success'] === false) {
-            return $this->msgOut(false, [], $picData['code']);
-        }
-        //上传缩略图
-        $thumbnail = $ImageClass->creatThumbnail($picData['path'], 100, 200, 'sm_');
-        try {
-            $editData = $this->inputs;
-            unset($editData['id']);
-            unset($editData['pic']);
+            if ($picData['success'] === false) {
+                return $this->msgOut(false, [], $picData['code']);
+            }
+            //上传缩略图
+            $thumbnail = $ImageClass->creatThumbnail($picData['path'], 100, 200, 'sm_');
             $editData['pic_path'] = '/' . $picData['path'];
             $editData['thumbnail_path'] = '/' . $thumbnail;
+        }
+        try {
             $this->editAssignment($pastData, $editData);
             $pastData->save();
             return $this->msgOut(true);
@@ -142,25 +152,77 @@ class HomepageRotationChartController extends BackEndApiMainController
         if ($validator->fails()) {
             return $this->msgOut(false, [], '400', $validator->errors()->first());
         }
-        $pastData = $this->eloqM::find($this->inputs['id']);
-        if (is_null($pastData)) {
+        $pastDataEloq = $this->eloqM::find($this->inputs['id']);
+        if (is_null($pastDataEloq)) {
             return $this->msgOut(false, [], '101806');
         }
+        $pastData = $pastDataEloq;
+        DB::beginTransaction();
         try {
             $ImageClass = new Image();
-            $deleteStatus = $ImageClass->deletePic(substr($pastData['pic_path'], 1));
+            $deleteStatus = $ImageClass->deletePic(substr($pastDataEloq['pic_path'], 1));
             if ($deleteStatus === false) {
                 return $this->msgOut(false, [], '101805');
             }
-            $pastData->delete();
+            $pastDataEloq->delete();
+            //往后的sort重新排序
+            $this->eloqM::where('sort', '>', $pastData->sort)->decrement('sort');
+            DB::commit();
             return $this->msgOut(true);
         } catch (Exception $e) {
+            DB::rollback();
             $errorObj = $e->getPrevious()->getPrevious();
             [$sqlState, $errorCode, $msg] = $errorObj->errorInfo; //［sql编码,错误妈，错误信息］
             return $this->msgOut(false, [], $sqlState, $msg);
         }
     }
 
+    public function sort()
+    {
+        $validator = Validator::make($this->inputs, [
+            'front_id' => 'required|numeric|gt:0',
+            'rearways_id' => 'required|numeric|gt:0',
+            'front_sort' => 'required|numeric|gt:0',
+            'rearways_sort' => 'required|numeric|gt:0',
+            'sort_type' => 'required|in:1,2',
+        ]);
+        if ($validator->fails()) {
+            return $this->msgOut(false, [], '400', $validator->errors()->first());
+        }
+        $pastFrontData = $this->eloqM::find($this->inputs['front_id']);
+        $pastRearwaysData = $this->eloqM::find($this->inputs['rearways_id']);
+        if (is_null($pastFrontData) || is_null($pastRearwaysData)) {
+            return $this->msgOut(false, [], '101807');
+        }
+        DB::beginTransaction();
+        try {
+            //上拉排序
+            if ($this->inputs['sort_type'] == 1) {
+                $stationaryData = $this->eloqM::find($this->inputs['front_id']);
+                $stationaryData->sort = $this->inputs['front_sort'];
+                $this->eloqM::where(function ($query) {
+                    $query->where('sort', '>=', $this->inputs['front_sort'])
+                        ->where('sort', '<', $this->inputs['rearways_sort']);
+                })->increment('sort');
+            } elseif ($this->inputs['sort_type'] == 2) {
+                //下拉排序
+                $stationaryData = $this->eloqM::find($this->inputs['rearways_id']);
+                $stationaryData->sort = $this->inputs['rearways_sort'];
+                $this->eloqM::where(function ($query) {
+                    $query->where('sort', '>', $this->inputs['front_sort'])
+                        ->where('sort', '<=', $this->inputs['rearways_sort']);
+                })->decrement('sort');
+            }
+            $stationaryData->save();
+            DB::commit();
+            return $this->msgOut(true);
+        } catch (\Exception $e) {
+            DB::rollback();
+            $errorObj = $e->getPrevious()->getPrevious();
+            [$sqlState, $errorCode, $msg] = $errorObj->errorInfo; //［sql编码,错误码，错误信息］
+            return $this->msgOut(false, [], $sqlState, $msg);
+        }
+    }
     //操作轮播图时获取的活动列表
     public function activityList()
     {
--- app/models/HomepageRotationChart.php
+++ app/models/HomepageRotationChart.php
@@ -7,6 +7,6 @@ class HomepageRotationChart extends BaseModel
     protected $table = 'homepage_rotation_chart';
 
     protected $fillable = [
-        'title', 'content', 'pic_path', 'thumbnail_path', 'type', 'redirect_url', 'activity_id', 'status', 'start_time', 'end_time', 'created_at', 'updated_at',
+        'title', 'content', 'pic_path', 'thumbnail_path', 'type', 'redirect_url', 'activity_id', 'status', 'start_time', 'end_time', 'sort', 'created_at', 'updated_at',
     ];
 }
