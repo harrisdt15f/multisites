4f4bf90fb6d6fce438860c7e9e904f233cfccd63
LingPh <953909858@qq.com>
2019-05-21 21:53:34 +0800

    前台 首页模块API

---
+++ app/Http/Controllers/FrontendApi/Homepage/HomepageController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\FrontendApi\Homepage;
+
+use App\Http\Controllers\FrontendApi\FrontendApiMainController;
+use App\models\ActivityInfos;
+use App\models\HomepageRotationChart;
+use App\models\PopularLotteries;
+use Illuminate\Support\Facades\Validator;
+
+class HomepageController extends FrontendApiMainController
+{
+    protected $eloqM = 'HomepageModel';
+
+    //需要展示的前台模块
+    public function showHomepageModel()
+    {
+        $showHomepageModel = $this->eloqM::select('key')->where('status', 1)->get();
+        return $this->msgOut(true, $showHomepageModel);
+    }
+
+    //轮播图
+    public function banner()
+    {
+        $data = HomepageRotationChart::select('id', 'title', 'pic_path', 'content', 'type', 'redirect_url', 'activity_id')
+            ->with(['activity' => function ($query) {
+                $query->select('id', 'redirect_url');
+            }])
+            ->where('status', 1)->get();
+        return $this->msgOut(true, $data);
+    }
+
+    //热门彩种一
+    public function popularLotteriesOne()
+    {
+        $showNum = $this->eloqM::select('show_num')->where('key', 'popularLotteries.one')->first();
+        $data = PopularLotteries::select('id', 'lotteries_id', 'pic_path')->with(['lotteries' => function ($query) {
+            $query->select('id', 'day_issue');
+        }])->where('type', 1)->limit($showNum->show_num)->get();
+        return $this->msgOut(true, $data);
+    }
+
+    //热门彩种二
+    public function popularLotteriesTwo()
+    {
+        $showNum = $this->eloqM::select('show_num')->where('key', 'popularLotteries.two')->first();
+        $data = PopularLotteries::select('id', 'lotteries_id')->with(['lotteries' => function ($query) {
+            $query->select('id', 'cn_name');
+        }])->where('type', 2)->limit($showNum->show_num)->get();
+        return $this->msgOut(true, $data);
+    }
+
+    //二维码
+    public function qrCode()
+    {
+        $qrCode = $this->eloqM::select('value')->where('key', 'qr.code')->get();
+        return $this->msgOut(true, $qrCode);
+    }
+
+    //活动
+    public function activity()
+    {
+        $showNum = $this->eloqM::select('show_num')->where('key', 'activity')->first();
+        $data = ActivityInfos::select('id', 'title', 'content', 'thumbnail_path', 'redirect_url')->where('status', 1)->orderBy('sort', 'asc')->limit($showNum->show_num)->get();
+        return $this->msgOut(true, $data);
+    }
+}
---
+++ routes/frontend/homepage.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * author: Alvin
+ * Date: 5/21/2019
+ * Time: 16:27 PM
+ */
+
+//首页
+Route::group(['prefix' => 'homepage', 'namespace' => 'Homepage'], function () {
+    $namePrefix = 'web-api.HomepageController.';
+    $controller = 'HomepageController@';
+    //首页-需要展示的模块
+    Route::match(['get', 'options'], 'show-homepage-model', ['as' => $namePrefix . 'show-homepage-model', 'uses' => $controller . 'showHomepageModel']);
+    //轮播图
+    Route::match(['get', 'options'], 'banner', ['as' => $namePrefix . 'banner', 'uses' => $controller . 'banner']);
+    //热门彩种一
+    Route::match(['get', 'options'], 'popular-lotteries-one', ['as' => $namePrefix . 'popular-lotteries-one', 'uses' => $controller . 'popularLotteriesOne']);
+    //热门彩种二
+    Route::match(['get', 'options'], 'popular-lotteries-two', ['as' => $namePrefix . 'popular-lotteries-two', 'uses' => $controller . 'popularLotteriesTwo']);
+    //二维码
+    Route::match(['get', 'options'], 'qrcode', ['as' => $namePrefix . 'qrcode', 'uses' => $controller . 'qrCode']);
+    //热门活动
+    Route::match(['get', 'options'], 'activity', ['as' => $namePrefix . 'activity', 'uses' => $controller . 'activity']);
+});
