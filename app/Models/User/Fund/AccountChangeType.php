238569641a8c00fd35e354f85763853db72011b8
harrisdt15f <harrisdt15f@gmail.com>
2019-06-05 15:05:31 +0800

    :sparkles: logging for frontend

--- app/Models/User/Fund/AccountChangeType.php
+++ app/Models/User/Fund/AccountChangeType.php
@@ -12,23 +12,36 @@ class AccountChangeType extends BaseModel
     protected $table = 'account_change_type';
 
     protected $fillable = [
-        'name', 'sign', 'in_out', 'type',
+        'name',
+        'sign',
+        'in_out',
+        'amount, user_id',
+        'project_id',
+        'lottery_id',
+        'method_id',
+        'issue',
+        'from_id',
+        'from_admin_id',
+        'to_id',
+        'frozen_type',
+        'activity_sign',
+        'admin_id'
     ];
 
     public static $rules = [
-        'name'              => 'required|min:2|max:32',
-        'sign'              => 'required|min:2|max:32',
-        'type'              => 'required|in:1,2',
-        'amount'            => 'required|in:0,1',
-        'user_id'           => 'required|in:0,1',
-        'project_id'        => 'required|in:0,1',
-        'lottery_id'        => 'required|in:0,1',
-        'method_id'         => 'required|in:0,1',
-        'issue'             => 'required|in:0,1',
-        'from_id'           => 'required|in:0,1',
-        'from_admin_id'     => 'required|in:0,1',
-        'to_id'             => 'required|in:0,1',
-        'frozen_type'       => 'required|in:0,1',
-        'activity_sign'     => 'required|in:0,1',
+        'name' => 'required|min:2|max:32',
+        'sign' => 'required|min:2|max:32',
+        'in_out' => 'required|in:1,2',
+        'amount' => 'required|in:0,1',
+        'user_id' => 'required|in:0,1',
+        'project_id' => 'required|in:0,1',
+        'lottery_id' => 'required|in:0,1',
+        'method_id' => 'required|in:0,1',
+        'issue' => 'required|in:0,1',
+        'from_id' => 'required|in:0,1',
+        'from_admin_id' => 'required|in:0,1',
+        'to_id' => 'required|in:0,1',
+        'frozen_type' => 'required|in:0,1',
+        'activity_sign' => 'required|in:0,1',
     ];
 }
