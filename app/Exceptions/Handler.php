46d4fdb52049106f53fbbf7180fa36e19a4daee4
harrisdt15f <harrisdt15f@gmail.com>
2019-06-07 11:42:50 +0800

    :sparkles: telegram error handling have been finished

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -13,28 +13,9 @@ use RicardoFontanelli\LaravelTelegram\Telegram;
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  Exception  $exception
      * @return void
      */
     public function report(Exception $exception)
@@ -93,19 +74,6 @@ class Handler extends ExceptionHandler
         );
 //        parent::report($exception);
     }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
-    }
-
 
     protected function unauthenticated($request, AuthenticationException $exception)
     {
