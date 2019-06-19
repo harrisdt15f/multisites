711ada70e06390b16d553c1d078222b57fde7aa5
harrisdt15f <harrisdt15f@gmail.com>
2019-06-19 17:40:11 +0800

    :sparkles: system log changes to guarded format

--- app/Models/Admin/FrontendSystemLog.php
+++ app/Models/Admin/FrontendSystemLog.php
@@ -12,39 +12,6 @@ class FrontendSystemLog extends BaseModel
     public const MOBILE = 4;
     public const TABLET = 5;
     public const OTHER = 6;
-
-    protected $fillable = [
-        'description',
-        'log_uuid',
-        'origin',
-        'type',
-        'result',
-        'level',
-        'token',
-        'ip',
-        'ips',
-        'admin_id',
-        'admin_name',
-        'user_id',
-        'username',
-        'user_agent',
-        'session',
-        'lang',
-        'device',
-        'os',
-        'os_version',
-        'browser',
-        'bs_version',
-        'device_type',
-        'inputs',
-        'route',
-        'route_id',
-        'menu_id',
-        'menu_label',
-        'menu_path',
-        'created_at',
-        'updated_at',
-    ];
     /**
      * @var array $guarded
      */
