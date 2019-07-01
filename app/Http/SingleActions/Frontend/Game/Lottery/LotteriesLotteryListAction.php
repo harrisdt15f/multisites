fd6a347787f6f3185ef11cbaee4dc562d6d5c80a
harrisdt15f <harrisdt15f@gmail.com>
2019-07-01 11:04:15 +0800

    only get the opening status lottery

--- app/Http/SingleActions/Frontend/Game/Lottery/LotteriesLotteryListAction.php
+++ app/Http/SingleActions/Frontend/Game/Lottery/LotteriesLotteryListAction.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @Author: LingPh
- * @Date:   2019-06-24 21:43:49
- * @Last Modified by:   LingPh
- * @Last Modified time: 2019-06-27 15:27:48
- */
 namespace App\Http\SingleActions\Frontend\Game\Lottery;
 
 use App\Http\Controllers\FrontendApi\FrontendApiMainController;
@@ -27,18 +21,19 @@ class LotteriesLotteryListAction
         if ($maxMultiples === null) {
             $maxMultiples = $this->createMaxMultiplesConfig();
         }
-        $lotteries = LotteryList::with(['issueRule:lottery_id,begin_time,end_time'])->get([
-            'cn_name as name',
-            'en_name',
-            'series_id',
-            'min_times',
-            'max_times',
-            'valid_modes',
-            'min_prize_group',
-            'max_prize_group',
-            'max_trace_number',
-            'day_issue',
-        ]);
+        $lotteries = LotteryList::with(['issueRule:lottery_id,begin_time,end_time'])
+            ->where('status', 1)->get([
+                'cn_name as name',
+                'en_name',
+                'series_id',
+                'min_times',
+                'max_times',
+                'valid_modes',
+                'min_prize_group',
+                'max_prize_group',
+                'max_trace_number',
+                'day_issue',
+            ]);
         $seriesConfig = config('game.main.series');
         $data = [];
         foreach ($lotteries as $lottery) {
@@ -75,7 +70,8 @@ class LotteriesLotteryListAction
             $systemELoq = $configurationsRelated->create(0, 'system', '系统相关', '所有系统相关配置都保存此', null, 1, 1);
             $parentId = $systemELoq->id;
         }
-        $maxMultiplesEloq = $configurationsRelated->create($parentId, 'max_multiples', '投注最大倍数', '玩家投注时可选择的最大倍数', 10000, 1, 1);
+        $maxMultiplesEloq = $configurationsRelated->create($parentId, 'max_multiples', '投注最大倍数', '玩家投注时可选择的最大倍数', 10000,
+            1, 1);
         return $maxMultiplesEloq->value;
     }
 }
