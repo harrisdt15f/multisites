d5a4979d162da062c773abaeca3a39dc85611f8b
harrisdt15f <harrisdt15f@gmail.com>
2019-07-08 16:18:09 +0800

    :sparkles: 投注全包修复 json 字符串问题 解决

--- app/Rules/Frontend/Lottery/Bet/MethodCountsRule.php
+++ app/Rules/Frontend/Lottery/Bet/MethodCountsRule.php
@@ -4,6 +4,7 @@ namespace App\Rules\Frontend\Lottery\Bet;
 
 use App\Models\Game\Lottery\LotteryMethod;
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Log;
 
 class MethodCountsRule implements Rule
 {
@@ -29,11 +30,28 @@ class MethodCountsRule implements Rule
     public function passes($attribute, $value)
     {
         preg_match('/\d+/', $attribute, $matches);
-        $methodId = $this->method_id[$matches[0]]['method_id'];
-        $count = $this->method_id[$matches[0]]['count'];
-        $oMethodRaws = LotteryMethod::where('method_id', $methodId)->first();
-        if ($count > $oMethodRaws->total) {
-            $this->message = '注数大于全包注数';
+        try {
+            $methodId = $this->method_id[$matches[0]]['method_id'];
+            $count = $this->method_id[$matches[0]]['count'];
+            $oMethodRaws = LotteryMethod::where('method_id', $methodId)->first();
+            if ($count > $oMethodRaws->total) {
+                $this->message = '注数大于全包注数';
+                return false;
+            }
+        } catch (\Exception $e) {
+            if (!empty($this->method_id)) {
+                $arrMethod = json_decode($this->method_id, true);
+                $methodId = $arrMethod[$matches[0]]['method_id'];
+                $count = $arrMethod[$matches[0]]['count'];
+                $oMethodRaws = LotteryMethod::where('method_id', $methodId)->first();
+                if ($count > $oMethodRaws->total) {
+                    $this->message = '注数大于全包注数';
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+            Log::error($e->getMessage().$e->getTraceAsString().$attribute);
             return false;
         }
         return true;
