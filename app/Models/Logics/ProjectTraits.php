2ab24b3a0a6645ee9d9c072d66f7a80b31b57ce9
LingPh <953909858@qq.com>
2019-07-13 11:58:00 +0800

    删除没用的  ProjectTraits   getList  function

--- app/Models/Logics/ProjectTraits.php
+++ app/Models/Logics/ProjectTraits.php
@@ -13,33 +13,6 @@ use Illuminate\Support\Facades\Request;
 
 trait ProjectTraits
 {
-
-    /**
-     * 前/后 台获取数据标准模板
-     * @param  $condition
-     * @return array
-     */
-    public static function getList($condition): array
-    {
-        $query = self::orderBy('id', 'desc');
-        if (isset($condition['en_name'])) {
-            $query->where('en_name', '=', $condition['en_name']);
-        }
-        $currentPage = isset($condition['page_index']) ? (int) $condition['page_index'] : 1;
-        $pageSize = isset($condition['page_size']) ? (int) $condition['page_size'] : 15;
-        $offset = ($currentPage - 1) * $pageSize;
-
-        $total = $query->count();
-        $menus = $query->skip($offset)->take($pageSize)->get();
-
-        return [
-            'data' => $menus,
-            'total' => $total,
-            'currentPage' => $currentPage,
-            'totalPage' => (int) ceil($total / $pageSize),
-        ];
-    }
-
     /**
      * @param $user
      * @param $lottery
