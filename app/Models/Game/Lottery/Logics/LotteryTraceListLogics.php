cf6218bfc518618f5b0c894cbbc19dad9a3aa8b8
LingPh <953909858@qq.com>
2019-07-25 20:30:57 +0800

    取消追号API 代码优化

--- app/Models/Game/Lottery/Logics/LotteryTraceListLogics.php
+++ app/Models/Game/Lottery/Logics/LotteryTraceListLogics.php
@@ -6,6 +6,6 @@ trait LotteryTraceListLogics
 {
     public function getUnfinishedTrace($traceId, $userId)
     {
-        return $this::where([['trace_id', $traceId], ['status', 0], ['user_id', $userId]])->get();
+        return $this::where([['trace_id', $traceId], ['status', self::STATUS_WAITING], ['user_id', $userId]])->get();
     }
 }
