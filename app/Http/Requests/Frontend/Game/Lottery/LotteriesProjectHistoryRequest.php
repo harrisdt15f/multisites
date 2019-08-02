ce52a9474a988ed755fd2e638435f45e44b4d3c6
LingPh <953909858@qq.com>
2019-08-02 22:23:30 +0800

    投注记录&追号记录  BUG修复

--- app/Http/Requests/Frontend/Game/Lottery/LotteriesProjectHistoryRequest.php
+++ app/Http/Requests/Frontend/Game/Lottery/LotteriesProjectHistoryRequest.php
@@ -26,9 +26,9 @@ class LotteriesProjectHistoryRequest extends BaseFormRequest
         //############################需要做验证处理
         return [
             // 'page_size' => 'string', //'integer|min:10|max:100',
-            'lottery_sign' => 'string', //'string|exists:lottery_lists,en_name',
-            'issue' => 'string', //'alpha_num',
-            'status' => 'string', //'',
+            // 'lottery_sign' => 'string', //'string|exists:lottery_lists,en_name',
+            // 'issue' => 'string', //'alpha_num',
+            // 'status' => 'string', //'',
         ];
     }
 
--- app/Http/Requests/Frontend/Game/Lottery/LotteriesTracesHistoryRequest.php
+++ app/Http/Requests/Frontend/Game/Lottery/LotteriesTracesHistoryRequest.php
@@ -26,9 +26,9 @@ class LotteriesTracesHistoryRequest extends BaseFormRequest
         //############################需要做验证处理
         return [
             // 'page_size' => 'string', //'integer|min:10|max:100',
-            'lottery_sign' => 'string', //'string|min:4|max:10|exists:lottery_lists,en_name',
-            'issue' => 'string', //'alpha_num',
-            'status' => 'string', //'',
+            // 'lottery_sign' => 'string', //'string|min:4|max:10|exists:lottery_lists,en_name',
+            // 'issue' => 'string', //'alpha_num',
+            // 'status' => 'string', //'',
         ];
     }
 
