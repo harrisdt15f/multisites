a0b43f97cebf3bf3adea880aaea9cebf58131787
harrisdt15f <harrisdt15f@gmail.com>
2019-08-11 21:39:46 +0800

    时时乐添加完成

--- app/Models/Game/Lottery/LotteryBasicMethod.php
+++ app/Models/Game/Lottery/LotteryBasicMethod.php
@@ -17,18 +17,20 @@ use App\models\Game\Lottery\Logics\SeriesLogic\Prizes\LottoPrize;
 use App\models\Game\Lottery\Logics\SeriesLogic\Prizes\P3p5Prize;
 use App\models\Game\Lottery\Logics\SeriesLogic\Prizes\SdPrize;
 use App\models\Game\Lottery\Logics\SeriesLogic\Prizes\SscPrize;
+use App\models\Game\Lottery\Logics\SeriesLogic\Prizes\SslPrize;
 use App\models\Game\Lottery\Logics\SeriesLogic\WinningNumber\K3BM;
 use App\models\Game\Lottery\Logics\SeriesLogic\WinningNumber\LottoBM;
 use App\models\Game\Lottery\Logics\SeriesLogic\WinningNumber\P3p5BM;
 use App\models\Game\Lottery\Logics\SeriesLogic\WinningNumber\SdBM;
 use App\models\Game\Lottery\Logics\SeriesLogic\WinningNumber\SscBM;
+use App\models\Game\Lottery\Logics\SeriesLogic\WinningNumber\SslBM;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LotteryBasicMethod extends BaseModel
 {
     use LotteryBasicMethodLogics;
-    use SscBM,LottoBM,K3BM,SdBM,P3p5BM;
-    use SscPrize,LottoPrize,K3Prize,SdPrize,P3p5Prize;
+    use SscBM,LottoBM,K3BM,SdBM,P3p5BM,SslBM;
+    use SscPrize,LottoPrize,K3Prize,SdPrize,P3p5Prize,SslPrize;
 
     protected $guarded = ['id'];
 
