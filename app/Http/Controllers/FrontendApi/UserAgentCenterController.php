a07e2d258f43e9e15e40ae4f542d7a94c9223dec
Three <jiangsan6688@gmail.com>
2019-08-12 20:43:09 +0800

    remove registerablelinkewquest

--- app/Http/Controllers/FrontendApi/UserAgentCenterController.php
+++ app/Http/Controllers/FrontendApi/UserAgentCenterController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\FrontendApi;
 
 use App\Http\Requests\Frontend\UserAgentCenter\{UserBonusRequest, UserDaysalaryRequest, UserProfitsRequest};
 use App\Http\SingleActions\Frontend\User\AgentCenter\{UserProfitsAction, UserDaysalaryAction, UserBonusAction};
-use App\Http\Requests\Frontend\UserAgentCenter\UserAgentCenterRegisterableLinkRequest;
 use App\Http\Requests\Frontend\UserAgentCenter\UserAgentCenterRegisterLinkRequest;
 use App\Http\SingleActions\Frontend\User\AgentCenter\UserAgentCenterRegisterableLinkAction;
 use App\Http\SingleActions\Frontend\User\AgentCenter\UserAgentCenterRegisterLinkAction;
@@ -38,13 +37,12 @@ class UserAgentCenterController extends FrontendApiMainController
 
     /**
      * 链接开户信息
-     * @param UserAgentCenterRegisterableLinkRequest $request
      * @param UserAgentCenterRegisterableLinkAction $action
      * @return JsonResponse
      */
-    public function RegisterableLink(UserAgentCenterRegisterableLinkRequest $request,UserAgentCenterRegisterableLinkAction $action):JsonResponse
+    public function RegisterableLink(UserAgentCenterRegisterableLinkAction $action):JsonResponse
     {
-        return $action->execute($this, $request);
+        return $action->execute($this);
     }
 
 
