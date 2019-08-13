fe96cbd0c668931311d081b0f873395a48fdce60
David <31339991@qq.com>
2019-08-13 10:45:25 +0800

    mobile-api 代理中心

---
+++ app/Http/Controllers/MobileApi/UserAgentCenterController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\MobileApi;
+
+use App\Http\Controllers\FrontendApi\FrontendApiMainController;
+use App\Http\Requests\Frontend\UserAgentCenter\{UserBonusRequest, UserDaysalaryRequest, UserProfitsRequest};
+use App\Http\SingleActions\Frontend\User\AgentCenter\{UserProfitsAction, UserDaysalaryAction, UserBonusAction};
+use App\Http\Requests\Frontend\UserAgentCenter\UserAgentCenterRegisterLinkRequest;
+use App\Http\SingleActions\Frontend\User\AgentCenter\UserAgentCenterRegisterableLinkAction;
+use App\Http\SingleActions\Frontend\User\AgentCenter\UserAgentCenterRegisterLinkAction;
+use Illuminate\Http\{JsonResponse};
+
+class UserAgentCenterController extends FrontendApiMainController
+{
+
+    /**
+     * 用户团队盈亏
+     * @param UserProfitsAction $action
+     * @param UserProfitsRequest $request
+     * @return JsonResponse
+     */
+    public function UserProfits(UserProfitsAction $action , UserProfitsRequest $request): JsonResponse
+    {
+        return $action->execute($this, $request);
+    }
+
+    /**
+     * 用户日工资
+     * @param UserDaysalaryAction $action
+     * @param UserDaysalaryRequest $request
+     * @return JsonResponse
+     */
+
+    public function UserDaysalary(UserDaysalaryAction $action , UserDaysalaryRequest $request): JsonResponse
+    {
+        return $action->execute($this, $request);
+    }
+
+    /**
+     * 链接开户信息
+     * @param UserAgentCenterRegisterableLinkAction $action
+     * @return JsonResponse
+     */
+    public function RegisterableLink(UserAgentCenterRegisterableLinkAction $action):JsonResponse
+    {
+        return $action->execute($this);
+    }
+
+
+    /**
+     * 生成开户链接
+     * @param UserAgentCenterRegisterLinkRequest $request
+     * @param UserAgentCenterRegisterLinkAction $action
+     * @return JsonResponse
+     */
+    public function RegisterLink(UserAgentCenterRegisterLinkRequest $request, UserAgentCenterRegisterLinkAction $action):JsonResponse
+    {
+        return $action->execute($this, $request->validated());
+    }
+
+    /**
+     * 用户分红
+     * @param UserBonusAction $action
+     * @param UserBonusRequest $request
+     * @return JsonResponse
+     */
+    public function UserBonus(UserBonusAction $action, UserBonusRequest $request) : JsonResponse
+    {
+        return $action->execute($this, $request);
+    }
+}
