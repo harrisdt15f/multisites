1055bc0c6a6234eb51d02c0a8e07a473a8ef9a5e
David <31339991@qq.com>
2019-08-14 15:21:41 +0800

    update

--- app/Models/User/Fund/Logics/UserAccountLogics.php
+++ app/Models/User/Fund/Logics/UserAccountLogics.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\User\Fund\Logics;
 
+use App\Jobs\UpdateUserProfits;
 use App\Lib\Clog;
 use App\Lib\Locker\AccountLocker;
 use App\Lib\Logic\AccountChange;
@@ -288,9 +289,8 @@ trait UserAccountLogics
 //            $accountChange->triggerSave();
             $accountLocker->release();
             //推入消息队列处理更新用户盈亏
-            Artisan::call('UpdateUserProfits', [
-                'userId' => $this->user->id,
-            ]);
+            dispatch(new UpdateUserProfits($this->user->id));
+
             return true;
         } catch (Exception $e) {
             $accountLocker->release();
