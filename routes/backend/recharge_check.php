b71834be4f4b19453ca70c45976e992b007cb315
David <31339991@qq.com>
2019-08-30 11:13:04 +0800

    后台提现、充值列表

--- routes/backend/recharge_check.php
+++ routes/backend/recharge_check.php
@@ -6,4 +6,7 @@ Route::group(['prefix' => 'recharge-check', 'namespace' => 'Users\Fund'], functi
     Route::match(['post', 'options'], 'detail', ['as' => $namePrefix . 'detail', 'uses' => $controller . 'detail']);
     Route::match(['post', 'options'], 'audit-success', ['as' => $namePrefix . 'audit-success', 'uses' => $controller . 'auditSuccess']);
     Route::match(['post', 'options'], 'audit-failure', ['as' => $namePrefix . 'audit-failure', 'uses' => $controller . 'auditFailure']);
+    //后台充值列表
+    Route::match(['post', 'options'], 'recharge-list', ['as' => $namePrefix . 'recharge-list',
+        'uses' => $controller . 'backRechargeList']);
 });
