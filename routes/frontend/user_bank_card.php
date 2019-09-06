2b0ee7083de08f791c87910e24d85176acb32985
park <myvtctt@gmail.com>
2019-09-06 16:18:56 +0800

    更该二次添加银行卡验证

--- routes/frontend/user_bank_card.php
+++ routes/frontend/user_bank_card.php
@@ -1,12 +1,12 @@
 <?php
 
-Route::group(['prefix' => 'user-bank-card', 'namespace' => 'User\Fund'], function () {
+Route::group(['prefix' => 'user-bank-card', 'namespace' => 'User\Fund'], static function () {
     $namePrefix = 'web-api.UserBankCardController.';
     $controller = 'UserBankCardController@';
     //用户银行卡列表
     Route::match(['get', 'options'], 'lists', ['as' => $namePrefix . 'lists', 'uses' => $controller . 'lists']);
     //用户添加绑定银行卡
-    Route::match(['post', 'options'], 'add', ['as' => $namePrefix . 'add', 'uses' => $controller . 'add']);
+    Route::match(['post', 'options'], 'add', ['as' => $namePrefix . 'add', 'uses' => $controller . 'addCard']);
     //用户删除绑定银行卡
     Route::match(['post', 'options'], 'delete', ['as' => $namePrefix . 'delete', 'uses' => $controller . 'delete']);
     //添加银行卡时选择的银行列表
@@ -15,4 +15,6 @@ Route::group(['prefix' => 'user-bank-card', 'namespace' => 'User\Fund'], functio
     Route::match(['get', 'options'], 'province-lists', ['as' => $namePrefix . 'province-lists', 'uses' => $controller . 'provinceLists']);
     //添加银行卡时选择的城市列表
     Route::match(['post', 'options'], 'city-lists', ['as' => $namePrefix . 'city-lists', 'uses' => $controller . 'cityLists']);
+    //二次添加银行卡验证资金密码与拥有者
+    Route::match(['post', 'options'], 'two-add-verifiy', ['as' => $namePrefix . 'two-add-verifiy', 'uses' => $controller . 'twoAddVerifiy']);
 });
